ebb00c135a03ee35242366202995aec194a4d8e1
Murilo20H <124621314+Murilo20H@users.noreply.github.com>
2023-04-29 12:59:33 -0300

    Modulo 2: desenvolvimento web

---
+++ Modulo_2_Desenvolvimento_Web/Listas/Lista_5/Lista5_exercicio1.js
@@ -0,0 +1,53 @@
+let pessoa1 = 
+{
+    nome: "Jorge",
+    idade: 24,
+    ehAdmin: true,
+    email: "jorge12345@hotmail.com"
+  
+}
+let pessoa2 = 
+{
+    nome: "Jorgeson",
+    idade: 25,
+    ehAdmin: true,
+    email: "jmsilva6589@hotmail.com"
+  
+}
+let pessoa3 = 
+{
+    nome: "Ana",
+    idade: 21,
+    ehAdmin: false,
+    email: "ana.maria.2002@hotmail.com"
+  
+}
+let pessoa4 = 
+{
+    nome: "John",
+    idade: 19,
+    ehAdmin: false,
+    email: "johnjohn2004@hotmail.com"
+  
+}
+let pessoa5 = 
+{
+    nome: "Henrique",
+    idade: 32,
+    ehAdmin: false,
+    email: "hrs87492645@hotmail.com"
+  
+}
+let listaPessoas = [pessoa1, pessoa2, pessoa3, pessoa4, pessoa5]
+for (let i = 0; i < listaPessoas.length; i++) 
+{
+    if(listaPessoas[i].ehAdmin)
+    {
+        console.log("Olá ", listaPessoas[i].nome, ", bem vindo(a) admin do sistema, enviamos um email para ", listaPessoas[i].email, " para você criar uma senha")
+    }
+    else
+    {
+        console.log("Olá ", listaPessoas[i].nome, ", bem vindo(a) usuário do sistema, enviamos um email para ", listaPessoas[i].email, " para você criar uma senha")
+    }
+    
+}
